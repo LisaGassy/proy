7c9eba7ca9778ed1a27c028c5daa032184542f43
LisaGassy <femida@mail.ru>
2019-12-24 15:51:34 +0300

    gf

--- kr/src/app/aaa/aaa.component.ts
+++ kr/src/app/aaa/aaa.component.ts
@@ -6,17 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aaa.component.css']
 })
 export class AaaComponent implements OnInit {
-  login:string;
-  password:string;
+  login: string;
+  password: string;
+  entered: boolean;
   constructor() { }
 
   ngOnInit() {
-    this.login='admin1'
-    this.password='admin2'
+    this.login = 'admin1'
+    this.password = 'admin2'
+    this.entered = false
   }
-  MakeLogin(){
-    if(this.login === 'admin' && this.password==='admin'){
+  MakeLogin() {
+    if (this.login === 'admin' && this.password === 'admin') {
       console.log('all ok');
-  }
+      this.entered = true
+    }
+    else if (this.login !== 'admin' && this.password !== 'admin') {
+      const pas = localStorage.getItem('password')
+      const log = localStorage.getItem('login');
+      if (log === this.login && pas === this.password) {
+        console.log('all ok');
+        this.entered = true
+      }
+
+      else {
+        this.entered = false;
+        localStorage.setItem('login', this.login);
+        localStorage.setItem('password', this.password);
+      }
+    }
   }
 }
